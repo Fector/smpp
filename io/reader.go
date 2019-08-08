a55249444ffd0987bf253560088e60cbb2012e59
DeathHand <stmaxonline@gmail.com>
2019-08-08 13:18:37 +0300

    String reader

--- io/reader.go
+++ io/reader.go
@@ -1,16 +1,87 @@
 package io
 
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"strings"
+)
+
 type Reader struct {
 }
 
-func (r *Reader) readInt() uint32 {
-	return 0
+func (r *Reader) readInt(buffer *bytes.Buffer) (uint32, error) {
+	b := make([]byte, 4)
+
+	if buffer.Len() < len(b) {
+		return 0, errors.New("Not enough length for 4-byte int ")
+	}
+
+	l, err := buffer.Read(b)
+
+	if l < len(b) {
+		return 0, errors.New("Unable to read 4 bytes for int ")
+	}
+
+	if err != nil {
+		return 0, err
+	}
+
+	return binary.BigEndian.Uint32(b), nil
 }
 
-func (r *Reader) readOctString() string {
-	return ""
+func (r *Reader) readVarOctString(buffer *bytes.Buffer, length int) (string, error) {
+	var builder strings.Builder
+
+	for i := 0; i < length; i++ {
+		b := make([]byte, 1)
+
+		l, err := buffer.Read(b)
+
+		if l < 1 {
+			return "", errors.New("Unable to read 1 byte for string builder ")
+		}
+
+		if err != nil {
+			return "", err
+		}
+
+		if b[0] == 0 {
+			break
+		}
+
+		builder.Write(b)
+	}
+
+	return builder.String(), nil
 }
 
-func (r *Reader) readString() string {
+func (r *Reader) readFixedOctString(buffer *bytes.Buffer, length int) (string, error) {
+	var builder strings.Builder
+
+	for i := 0; i < length; i++ {
+		b := make([]byte, 1)
+
+		l, err := buffer.Read(b)
+
+		if err != nil {
+			return "", err
+		}
+
+		if l < len(b) {
+			return "", errors.New("Unable to read 1 byte for string builder ")
+		}
+
+		builder.Write(b)
+
+		if b[0] == 0 {
+			break
+		}
+	}
+
+	return builder.String(), nil
+}
+
+func (r *Reader) readString(b *bytes.Buffer) string {
 	return ""
 }
