4afacb65cb4d50464b17f9a6b8a1c4c5003b2b1d
DeathHand <stmaxonline@gmail.com>
2019-08-17 10:07:05 +0300

    SMPP v3.4 bind mode short names

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -5,3 +5,8 @@ const PduHeaderLength uint32 = 16
 
 // SMPP v3.4 - 5.2.13 page 123
 const ProtocolId uint32 = 0x34
+
+// SMPP v3.4 bind mode short names
+const BindModeTX string = "TX"
+const BindModeRX string = "RX"
+const BindModeTRX string = "TRX"
